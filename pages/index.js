1fab28306fe26a5a7a4abf1dd8024e55812cf26c
Park JunYong <dyden119@gmail.com>
2024-02-15 17:37:35 +0900

    feat: 서치,메인에 상품 리스트 추가

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
+import { useEffect, useState } from 'react';
+import axios from '@/lib/axios';
+import ProductList from '@/components/ProductList';
 import SearchForm from '@/components/SearchForm';
-import styles from '@/styles/Home.module.css';
-import Link from 'next/link';
 
 export default function Home() {
+  const [products, setProducts] = useState();
+
+  async function getProducts() {
+    const res = await axios.get(`/products/`);
+    const nextProduct = res.data.results;
+    setProducts(nextProduct);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <div>
+    <>
       <h1>Codeitmall</h1>
       <SearchForm />
-      <ul>
-        <li>
-          <Link href="/products/1">첫 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="/products/2">두 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="/products/3">세 번째 상품</Link>
-        </li>
-      </ul>
-    </div>
+      <ProductList products={products} />
+    </>
   );
 }
--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,30 @@
-import SearchForm from "@/components/SearchForm";
-import { useRouter } from "next/router";
+import { useState, useEffect } from 'react';
+import SearchForm from '@/components/SearchForm';
+import axios from '@/lib/axios';
+import ProductList from '@/components/ProductList';
+import { useRouter } from 'next/router';
 
-export default function Search(){
+export default function Search() {
+  const [products, setProducts] = useState();
   const router = useRouter();
-  const {q} = router.query;
+  const { q } = router.query;
+
+  async function getProducts(query) {
+    const res = await axios.get(`/products/?q=${query}`);
+    const nextProduct = res.data.results;
+    setProducts(nextProduct);
+  }
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
 
   return (
     <div>
       <h1>Search 페이지</h1>
-      <SearchForm initialValue={q}/>
+      <SearchForm initialValue={q} />
       <h2>{q} 검색 결과</h2>
+      <ProductList products={products} />
     </div>
-  )
+  );
 }
