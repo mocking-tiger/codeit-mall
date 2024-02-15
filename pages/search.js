b78be2d119f92d59898fe0bf959d55e41c6beb83
Park JunYong <dyden119@gmail.com>
2024-02-15 14:28:51 +0900

    초기설정

---
+++ pages/search.js
@@ -0,0 +1,15 @@
+import SearchForm from "@/components/SearchForm";
+import { useRouter } from "next/router";
+
+export default function Search(){
+  const router = useRouter();
+  const {q} = router.query;
+
+  return (
+    <div>
+      <h1>Search 페이지</h1>
+      <SearchForm initialValue={q}/>
+      <h2>{q} 검색 결과</h2>
+    </div>
+  )
+}
